53f78493a52010751d1eca728a30b7a1c050f292
Riki Nolasco <ricardot.nolasco@pccw.com>
2022-11-18 16:15:05 +0800

    searchParams route

--- src/component/User.tsx
+++ src/component/User.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 export const User = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showActiveUser = searchParams.get("userid") === "active";
   return (
     <>
-      <h2>
-        <Outlet />
-      </h2>
+      <div>
+        <h2>
+          <Outlet />
+        </h2>
+        <button onClick={() => setSearchParams({ userid: "active" })}>Active User</button>
+        <button onClick={() => setSearchParams({})}>Reset</button>
+      </div>
+      {showActiveUser ? <h2>showing active users</h2> : <h2>Showing Users</h2>}
     </>
   );
 };
